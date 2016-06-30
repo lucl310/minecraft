61ceb5514d38a16e563cca7123a95a06b2d8daa3
Luc lambert <lucthehuman21@gmail.com>
2016-06-30 11:35:19 -1000

    added lots
    I added the background, i made it so that numbers show up in the red
    box, added some size resets

--- js/index.js
+++ js/index.js
@@ -1,9 +1,36 @@
-var Ore = ["Diamond", "gold", "gold", "iron", "iron", "iron", "cobblestone", 
-"cobblestone","cobblestone","cobblestone","cobblestone"];
+var Ore = ["Diamond", "Gold", "Gold", "Iron", "Iron", "Iron", "Cobble", 
+"Cobble","Cobble","Cobble","Cobble"];
 
-function pickOre(){
-var oreFound = Math.floor(Math.random()*Ore.length);
-document.getElementById('Pick').innerHTML = "<h1>" +"You just found "Ore[oreFound]+"</h1>";
-console.log(Ore[oreFound]);
-console.log(oreFound);
+var diamondCount = 0;
+var goldCount = 0;
+var ironCount = 0;
+var cobbleCount = 0;
+
+var press = function(){
+	var oreFound = Math.floor(Math.random()*Ore.length);
+	console.log(Ore[oreFound]);
+
+	if (Ore[oreFound] === "Diamond") {
+		diamondCount++;
+		console.log(diamondCount);
+		document.getElementById('diamondDisplay').innerHTML =diamondCount;
+	};
+	if (Ore[oreFound] === "Gold") {
+		goldCount++;
+		console.log(goldCount);
+		document.getElementById('goldDisplay').innerHTML =goldCount;
+	};
+	if (Ore[oreFound] === "Iron") {
+		ironCount++;
+		console.log(ironCount);
+		document.getElementById('ironDisplay').innerHTML =ironCount;
+	};
+		if (Ore[oreFound] === "Cobble") {
+		cobbleCount++;
+		console.log(cobbleCount);
+		document.getElementById('cobbleDisplay').innerHTML =cobbleCount;
+	};
+	document.getElementById('Pick').innerHTML = "<h1>You just found "+Ore[oreFound]+"</h1>";
+	
+	console.log(oreFound);
 }
